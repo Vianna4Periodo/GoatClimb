bef873431a2254a2c2c9704066cc0d1ad7a6a520
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-05-21 18:29:31 -0300

    Atualização background

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -21,6 +21,13 @@ var GameState = {
         this.points = 0;
         this.life = 3;
         this.maxPoints = 300;
+
+        this.max = 0;
+        this.ront_emitter = 0;
+        this.mid_emitter = 0;
+        this.back_emitter = 0;
+        this.update_interval = 4 * 60;
+        this.i = 0;
     },
 
     preload: function() {
@@ -30,6 +37,8 @@ var GameState = {
 
         // this.load.image('progress', 'assets/progress.png');
         this.load.image('background', 'assets/background.png');
+
+        this.load.image('snowflakes', 'assets/snowflakes.png');
     },
 
     create: function() {
@@ -39,6 +48,7 @@ var GameState = {
         this.platformsCreate();
         this.enemiesCreate();
         this.goatCreate();
+        // this.setupSnow();
 
         this.setupHUD();
     },
@@ -80,6 +90,14 @@ var GameState = {
 
         this.updatePoints();
         // this.updateLife();
+
+        // this.i++;
+        //
+        // if (this.i === this.update_interval) {
+        //     this.changeWindDirection();
+        //     this.update_interval = Math.floor(Math.random() * 4) * 20; // 0 - 20sec @ 60fps
+        //     this.i = 0;
+        // }
     },
 
     // MARK: - Public Methods
@@ -250,5 +268,71 @@ var GameState = {
     // updateLife: function() {
     //     this.lifeText.setText("Vidas: " + this.life + " / 3");
     // }
+
+    setupSnow: function() {
+        this.back_emitter = game.add.emitter(game.world.centerX, -32, 600);
+        this.back_emitter.makeParticles('snowflakes', [0, 1, 2, 3, 4, 5]);
+        this.back_emitter.maxParticleScale = 0.6;
+        this.back_emitter.minParticleScale = 0.2;
+        this.back_emitter.setYSpeed(20, 100);
+        this.back_emitter.gravity = 0;
+        this.back_emitter.width = game.world.width * 1.5;
+        this.back_emitter.minRotation = 0;
+        this.back_emitter.maxRotation = 40;
+
+        this.mid_emitter = game.add.emitter(game.world.centerX, -32, 250);
+        this.mid_emitter.makeParticles('snowflakes', [0, 1, 2, 3, 4, 5]);
+        this.mid_emitter.maxParticleScale = 1.2;
+        this.mid_emitter.minParticleScale = 0.8;
+        this.mid_emitter.setYSpeed(50, 150);
+        this.mid_emitter.gravity = 0;
+        this.mid_emitter.width = game.world.width * 1.5;
+        this.mid_emitter.minRotation = 0;
+        this.mid_emitter.maxRotation = 40;
+
+        this.front_emitter = game.add.emitter(game.world.centerX, -32, 50);
+        this.front_emitter.makeParticles('snowflakes', [0, 1, 2, 3, 4, 5]);
+        this.front_emitter.maxParticleScale = 1;
+        this.front_emitter.minParticleScale = 0.5;
+        this.front_emitter.setYSpeed(100, 200);
+        this.front_emitter.gravity = 0;
+        this.front_emitter.width = game.world.width * 1.5;
+        this.front_emitter.minRotation = 0;
+        this.front_emitter.maxRotation = 40;
+
+        this.changeWindDirection();
+
+        this.back_emitter.start(false, 14000, 20);
+        this.mid_emitter.start(false, 12000, 40);
+        this.front_emitter.start(false, 6000, 1000);
+    },
+
+    changeWindDirection: function() {
+
+        var multi = Math.floor((this.max + 200) / 4),
+            frag = (Math.floor(Math.random() * 100) - multi);
+        this.max = this.max + frag;
+
+        if (this.max > 200) this.max = 150;
+        if (this.max < -200) this.max = -150;
+
+        this.setXSpeed(this.back_emitter, this.max);
+        this.setXSpeed(this.mid_emitter, this.max);
+        this.setXSpeed(this.front_emitter, this.max);
+
+    },
+
+    setXSpeed: function(emitter, max) {
+
+        emitter.setXSpeed(max - 20, max);
+        emitter.forEachAlive(this.setParticleXSpeed, this, max);
+
+    },
+
+    setParticleXSpeed: function(particle, max) {
+
+        particle.body.velocity.x = max - Math.floor(Math.random() * 30);
+
+    },
 
 };
