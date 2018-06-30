883c3aa5de99d4f55b82769df4385679508eec3c
Adriano Souza Costa <adrianosouzacostaios@gmail.com>
2018-06-29 21:21:04 -0300

    Update SecondState.js

--- js/states/SecondState.js
+++ js/states/SecondState.js
@@ -3,46 +3,80 @@ var SecondState = {
     // MARK: - Life Cycle
 
     init: function() {
+        // Ajuste no scale para exibição no browser
         this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        
+        // Largura e altura máxima
         this.scale.maxWidth = this.game.width;
         this.scale.maxHeight = this.game.height;
+        
+        // Alinhamento horizontal e vertical
         this.scale.pageAlignHorizontally = true;
         this.scale.pageAlignVertically = true;
 
+        // Ativando fisica
         this.physics.startSystem(Phaser.Physics.ARCADE);
 
+        
         this.cameraYMin = 99999;
         this.platformYMin = 99999;
 
+        // Ativando uso do teclado
         this.cursors = this.game.input.keyboard.createCursorKeys();
 
+        // largura da plataforma
         this.platformBlockSize = 40;
+        
+        // força do pulo
         this.jumpForce = 500;
+        
+        // pontos iniciais
         this.points = 0;
+        
+        // vidas iniciais
         this.life = 3;
+        
+        // maximo de pontos para a fase
         this.maxPoints = 300;
     },
 
     preload: function() {
+        // carregando sprite da cabra
         this.load.spritesheet("goat", "assets/goat_jump_sprite.png", 200, 220, 5, 0, 10);
+        
+        // carregando sprite dos inimigos
         this.load.spritesheet("bird_monster", "assets/bird_monster.png", 111, 86, 4, 1, 4);
+        
+        // carregando sprite das vidas
         this.load.spritesheet('lifeHUD', 'assets/life.png', 99, 23, 3, 0, 5);
 
+        // carregando platafomas, base do stage e background
         this.load.image('platform', 'assets/platform_block.png');
         this.load.image('baseMountain', 'assets/base_mountain_2.png');
         this.load.image('background', 'assets/background_2.png');
 
+        // carregando audios
         this.load.audio('music', ['assets/sound/secondStateMusic.ogg']);
         this.load.audio('jumpMusic', ['assets/sound/jump.ogg']);
     },
 
     create: function() {
+        // Posiciona e inclui a imagend e base e background
         this.backgroundCreate();
-        this.platformsCreate();
+        
+        // Cria as plataformas base e iniciais
+        this.platformsCreateplatformsCreate();
+        
+        // cria o grupo para receber os inimigos
         this.enemiesCreate();
+        
+        // Cria o personagem principal
         this.goatCreate();
 
+        // criando efeitos sonoros
         this.setupMusic();
+        
+        // criando hud de pontuação e vidas
         this.setupHUD();
     },
 
@@ -56,31 +90,43 @@ var SecondState = {
     },
 
     update: function() {
+        // atribuindo um novo bounds para o mundo para fazer o efeito de descida
         this.world.setBounds(0, -this.goat.yChange, this.world.width, this.game.height + this.goat.yChange);
 
+        // Atualizando posição de referencia da câmera
         this.cameraYMin = Math.min(this.cameraYMin, this.goat.y - this.game.height + 130);
         this.camera.y = this.cameraYMin;
 
+        // Verificando colisão com as plataformas e inimigos
         this.physics.arcade.overlap(this.goat, this.birds, this.collisionEnemyHandler, null, this);
         this.physics.arcade.collide(this.goat, this.platforms, null, null, this);
 
+        // configurando e atualizando movimentação do personagem
         this.goatMovement();
 
+        // Analisando todas as plataformas ativas para poder criar as novas a partir com que o personagem vai subindo
         this.platforms.forEachAlive( function( elem ) {
             this.platformYMin = Math.min(this.platformYMin, elem.y);
 
+            // Se o elemnto sair do cenário ele é destruido
             if(elem.y > this.camera.y + this.game.height) {
                 elem.kill();
+                
+                // randomizando a aparição dos inimigos
                 var random = this.rnd.integerInRange(1, 10);
 
+                // caso o valor randomizado seja divisivel por 3, criamos um inimigo
                 if (random % 3 == 0) {
                     this.enemyCreate(this.platformYMin + 10);
                 }
-
+                
+                // criando a ptaforma utlizando largura e posição randomica
                 this.platformCreate(this.rnd.integerInRange(0, this.world.width - this.platformBlockSize), this.platformYMin - 100, this.rnd.integerInRange(1, 3));
             }
         }, this );
 
+        
+        // Atualizando pontuação do usuário
         this.updatePoints();
     },
 
